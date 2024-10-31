626e1c76406176801bdfd543f77f9ed1145ba44a
vaishakh3 <vichured@gmail.com>
2024-10-31 16:23:30 +0530

    initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+import './App.css'; // Make sure to import your CSS file
+
+export default function App() {
+  return (
+    <div className="fullscreen-container">
+      <Spline scene="https://prod.spline.design/f2owIh84Khj2mKNL/scene.splinecode" />
+    </div>
+  );
+}
